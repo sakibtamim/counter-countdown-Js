403f916c5d005fb41a7bc7dac341ccb9417a4d7a
sakibtamim <sakib.tamim@gmail.com>
2025-04-19 13:56:39 +0600

    3rd commit

--- js/count_down.js
+++ js/count_down.js
@@ -1,31 +1,52 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Apr 23, 2025 00:00:00").getTime();
+// var countDownDate = new Date("Apr 23, 2025 00:00:00").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function () {
-  // Get today's date and time
-  var now = new Date().getTime();
+// var x = setInterval(function () {
+//   var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+//   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById("demo1").innerHTML = days + "d ";
-  document.getElementById("demo2").innerHTML = hours + "h ";
-  document.getElementById("demo3").innerHTML = minutes + "m ";
-  document.getElementById("demo4").innerHTML = seconds + "s ";
+//   document.getElementById("demo1").innerHTML = days + "d ";
+//   document.getElementById("demo2").innerHTML = hours + "h ";
+//   document.getElementById("demo3").innerHTML = minutes + "m ";
+//   document.getElementById("demo4").innerHTML = seconds + "s ";
 
-  // If the count down is finished, write some text
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("demo1").innerHTML = "EXPIRED";
+//   }
+// }, 1000);
+
+let countDownDate = new Date("Apr 23 2025 00:00:00").getTime();
+
+function dateCountDown() {
+  let now = new Date().getTime();
+  let distance = countDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let secs = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.querySelector("#demo1").innerHTML = days + "d";
+  document.querySelector("#demo2").innerHTML = hours + "h";
+  document.querySelector("#demo3").innerHTML = mins + "m";
+  document.querySelector("#demo4").innerHTML = secs + "s";
+
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo1").innerHTML = "EXPIRED";
+    document.querySelector("#demo1").innerHTML = "expired";
+    document.querySelector("#demo2").innerHTML = "expired";
+    document.querySelector("#demo3").innerHTML = "expired";
+    document.querySelector("#demo4").innerHTML = "expired";
   }
+}
+let x = setInterval(() => {
+  dateCountDown();
 }, 1000);
